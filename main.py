93202930ff4e4faec11bfd1a275870a7ef56b093
STORMxCMD <usmonovscamn1@gmail.com>
2025-04-09 15:40:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from app import FrameWorkApp
+import json
 
-app=FrameWorkApp()
+app = FrameWorkApp()
+
+def load_users():
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    return users
 
 @app.route("/home")
 def home(request, response):
-    response.text="Home pagedan salom"
+    response.text = "Home pagedan salom!"
 
 @app.route("/about")
 def about(request, response):
-    response.text="About pagedan salom"
-    
+    response.text = "About pagedan salom!"
+
 @app.route("/contacts")
-def home(request, response):
-    response.text="Telegram - @storm_asliddin"
+def contacts(request, response):
+    response.text= "Telegram - @STORM_ASLIDDIN"
+
+@app.route("/u/id")
+def get_info(request, response, id):
+    users = load_users()
+    user = users.get(id, "Bunday user yo'q!")
+
+    response.text = json.dumps(user)
