c6322f8d406be11c33acff0416580d7996ebd2c1
STORMxCMD <usmonovscamn1@gmail.com>
2025-04-09 15:40:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,28 @@ from webob import Request, Response
 
 class FrameWorkApp:
     def __init__(self):
-        self.routes=dict()
-    
-    def __call__(self, environ, start_response):
-        req=Request(environ)
-        res=self.handle_request(req)
-        return res(environ, start_response) 
-    
-    def handle_request(self, request):
-        res=Response()
-        is_there=False
-        
-        for path, handler in self.routes.items():
-            if path == request.path:
-                handler(request, res)
-                is_there=True
+        self.routes = dict()
 
-        if not is_there:
-            self.default_response(res)
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+        res = self.handle_request(req)
+        return res(environ, start_response)
+
+    def handle_request(self, req):
+        res = Response()
+
+        for path, handler in self.routes.items():
+            lst = req.path.split("/")
+
+            if path == "/u/id" and len(lst) > 2:
+                handler(req, res, lst[2])
+
+            if path == req.path:
+                handler(req, res)
 
         return res
+    
+    
     def default_response(self, response):
         response.status_code=404
         response.text="URL hatolik bor, topilmadi"
